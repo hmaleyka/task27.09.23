0cbc19bc0512156a5c3a8f0055138a8159f065b4
Maleyka heybatova <maleykaheybatova1011@gmail.com>
2023-09-27 02:07:59 +0400

    the codes of task1 and task2 were added

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿//TASK1
+
+//int num = 10;
+//int num2 = 17;
+//int num3 = 20;
+
+//if (num % 5 == 0 && num2 % 5 == 0 && num3 % 5 == 0)
+//{
+//    Console.WriteLine(num + num2 + num3);
+//}
+//else if (num % 5 == 0 || num2 % 5 == 0 || num3 % 5 == 0)
+//{
+//    Console.WriteLine(num + num3);
+//}
+//else
+//{
+//    Console.WriteLine("the given values are not divided by 5");
+//}
+
+//TASK2
+
+//int number = 278;
+//int sum = 0;
+
+//int digit1 = number % 10;
+//sum += digit1;
+//number /= 10;
+
+//int digit2 = number % 10;
+//sum += digit2;
+//number /= 10;
+
+//int digit3 = number % 10;
+//sum += digit3;
+
+//Console.WriteLine($"The sum of digits of 278 is equal to: {sum}");
